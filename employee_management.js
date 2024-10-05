552b7a7ae79f51890a9ff34e6b5534df9fa00759
Kenzo Cogswell <kcogswell@usf.edu>
2024-10-04 21:48:38 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -2,14 +2,31 @@
 
 class Employee {
 constructor (name, salary, position, department) {
-    this.name = name
-    this.salary = salary
-    this.position = position
-    this.department = department
+    this.name = name;
+    this.salary = salary;
+    this.position = position;
+    this.department = department;
 }
 // now implementing the method
 getDetails() {
 console.log (`Name: ${this.name}, Position: ${this.position}, Salary: ${this.salary}`)
 }
 
+}
+
+// Task 2: Create a department class
+
+class Department {
+constructor (name) {
+    this.name = name;
+    this.employees = [];
+}
+// now implementing both methods: this first one adds the object
+addEmployee(employee) {        
+    this.employees.push (employee);
+}
+// this one adds up all salaries
+getDepartmentSalary() {
+    return this.employees.reduce((total, employee) => total + employee.salary, 0)
+}
 }
