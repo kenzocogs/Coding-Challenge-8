b5a54397e14bfcb29e45549a452d8c121a4dac76
Kenzo Cogswell <kcogswell@usf.edu>
2024-10-04 22:46:29 -0400

    Handle bonuses in the department salary calculation

--- employee_management.js
+++ employee_management.js
@@ -22,14 +22,26 @@ constructor (name) {
     this.employees = [];
 }
 // now implementing both methods: this first one adds the object
-addEmployee(employee) {        
+addEmployee(employee) {     
+    if (employee instanceof Employee)
     this.employees.push (employee);
+    else console.log (`${employee.name} is not a valid employee.`);
 }
 // this one adds up all salaries
 getDepartmentSalary() {
     return this.employees.reduce((total, employee) => total + employee.salary, 0)
 }
-}
+// Task 4: Handle bonuses for Managers
+
+calculateTotalSalaryWithBonus () {
+   return this.employees.reduce ((total, employee) => {
+    // checking if employee is a manager, then adding bonus if true
+        if (employee instanceof Manager) { 
+            return total + employee.salary + employee.bonus
+        }
+        return total += employee.salary;
+   }, 0);
+}}
 
 // Task 3: Create a manager class that inherits from Employee
 
